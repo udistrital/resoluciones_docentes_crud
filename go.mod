c49c8f4041718d36d449022aa1b3e70947e81a3e
anndresfelipe29 <afgomezs@correo.udistrital.edu.co>
2021-01-20 14:06:26 -0500

    feat: se dockeriza

--- go.mod
+++ go.mod
@@ -1,7 +1,8 @@
-
-module resoluciones_crud
+module github.com/udistrital/resoluciones_crud
 
 go 1.15
 
-require github.com/beego/beego/v2 v2.0.1
-require github.com/smartystreets/goconvey v1.6.4
+require (
+	github.com/beego/beego/v2 v2.0.1
+	github.com/lib/pq v1.9.0
+)
--- main.go
+++ main.go
@@ -1,19 +1,49 @@
 package main
 
 import (
-	_ "resoluciones_crud/routers"
+	"fmt"
 
-	beego "github.com/beego/beego/v2/server/web"
+	"github.com/beego/beego/v2"
 	"github.com/beego/beego/v2/client/orm"
+	"github.com/beego/beego/v2/plugins/cors"
+	beego "github.com/beego/beego/v2/server/web"
 	_ "github.com/lib/pq"
 )
 
 func main() {
-	orm.RegisterDataBase("default", "postgres", beego.AppConfig.String("sqlconn"))
+	//orm.RegisterDataBase("default", "postgres", beego.AppConfig.String("sqlconn"))
+
+	orm.RegisterDataBase("default", "postgres", "postgres://"+
+		LimpiarVariableentorno("PGuser")+":"+
+		LimpiarVariableentorno("PGpass")+"@"+
+		LimpiarVariableentorno("PGhost")+":"+
+		LimpiarVariableentorno("PGport")+"/"+
+		LimpiarVariableentorno("PGdb")+"?sslmode=disable&search_path="+
+		LimpiarVariableentorno("PGschema")+"")
+
 	if beego.BConfig.RunMode == "dev" {
 		beego.BConfig.WebConfig.DirectoryIndex = true
 		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
 	}
+
+	beego.InsertFilter("*", beego.BeforeRouter, cors.Allow(&cors.Options{
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{"PUT", "PATCH", "GET", "POST", "OPTIONS", "DELETE"},
+		AllowHeaders: []string{"Origin", "x-requested-with",
+			"content-type",
+			"accept",
+			"origin",
+			"authorization",
+			"x-csrftoken"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+	}))
+
 	beego.Run()
 }
 
+func LimpiarVariableentorno(key string) (respuesta string) {
+	respuesta, _ = beego.AppConfig.String(key)
+	fmt.Println(respuesta)
+	return
+}
