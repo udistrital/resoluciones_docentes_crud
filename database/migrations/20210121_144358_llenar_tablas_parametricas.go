3e1302474493b041eaaabd668cebbcad7a621490
Felipe <afgomezs@correo.udistrital.edu.co>
2021-02-04 14:44:16 -0500

    fix: se arreglan migraciones

--- database/migrations/20210121_144358_llenar_tablas_parametricas.go
+++ database/migrations/20210121_144358_llenar_tablas_parametricas.go
@@ -18,7 +18,7 @@ func init() {
 	m := &LlenarTablasParametricas_20210121_144358{}
 	m.Created = "20210121_144358"
 
-	migration.Register("LlenarTablasParametricas_20210121_144358", m)
+	migration.Register("LlenarTablasParametricasBd_20210121_144358", m)
 }
 
 // Run the migrations
@@ -27,16 +27,16 @@ func (m *LlenarTablasParametricas_20210121_144358) Up() {
 	file, err := ioutil.ReadFile("../scripts/20210121_144358_llenar_tablas_parametricas_up.sql")
 
 	if err != nil {
-  		// handle error
-  		fmt.Println(err)
+		// handle error
+		fmt.Println(err)
 	}
 
 	requests := strings.Split(string(file), ";")
 
 	for _, request := range requests {
-  		fmt.Println(request)
-  		m.SQL(request)
-  		// do whatever you need with result and error
+		fmt.Println(request)
+		m.SQL(request)
+		// do whatever you need with result and error
 	}
 }
 
@@ -46,15 +46,15 @@ func (m *LlenarTablasParametricas_20210121_144358) Down() {
 	file, err := ioutil.ReadFile("../scripts/20210121_144358_llenar_tablas_parametricas_down.sql")
 
 	if err != nil {
-  		// handle error
-  		fmt.Println(err)
+		// handle error
+		fmt.Println(err)
 	}
 
 	requests := strings.Split(string(file), ";")
 
 	for _, request := range requests {
-  		fmt.Println(request)
-  		m.SQL(request)
-  		// do whatever you need with result and error
+		fmt.Println(request)
+		m.SQL(request)
+		// do whatever you need with result and error
 	}
 }
